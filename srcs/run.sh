b5606039bb95214ff29e93ad481346dd7c5d3fc8
Hyun Jun Eom <zzp1318@gmail.com>
2021-04-12 21:29:47 +0900

    reallast

--- srcs/run.sh
+++ srcs/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 service mysql start
-mysql < /tmp/init.sql
+#mysql -u root --skip-password < /tmp/init.sql
 service php7.3-fpm start
-service nginx start
+##service nginx start
+#nginx -g "daemon off;" 
