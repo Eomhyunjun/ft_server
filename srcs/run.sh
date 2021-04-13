905bdc02b53d05ccfbced00a87d1596d645eeb73
Heom <zzp1318@gmail.com>
2021-04-13 12:46:36 +0900

    edit

--- srcs/run.sh
+++ srcs/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 service mysql start
-#mysql -u root --skip-password < /tmp/init.sql
+mysql -u root --skip-password < /tmp/init.sql
 service php7.3-fpm start
-##service nginx start
-#nginx -g "daemon off;" 
+nginx -g "daemon off;" 
